61da16199c91ed2badcb0101376e38e28bd873ba
丢失的橘子 <993033472@qq.com>
2023-04-07 14:03:48 +0800

    feat：新增组件集成，更新说明

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,34 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+//引入全局样式和主题
+import "./styles/default.css"
+//引入组件
+import ElementPlus from 'element-plus'
+//引入样式
+import 'element-plus/dist/index.css'
+//国际化声明
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+//引入图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+//引入mock
+import './mock/index.ts'
+//引入vuex
+import store from './store/index.js'
 
-createApp(App).use(router).mount('#app')
+//全局注册
+const app = createApp(App);
+//注册ElementPlus和国际化
+app.use(ElementPlus, {
+    locale: zhCn
+});
+//注册路由
+app.use(router);
+//遍历图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+//注册vuex
+app.use(store);
+//挂载
+app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+// 2. 定义一些路由
+// 每个路由都需要映射到一个组件。
 const routes = [
     {
         path: '/', // 路由路径
@@ -8,9 +10,12 @@ const routes = [
     }
 ];
 
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
 const router = createRouter({
     history: createWebHashHistory(),
-    routes
+    routes  // `routes: routes` 的缩写
 })
 
 export default router;
